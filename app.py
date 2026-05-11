1cdf820870effa08d93f18e6ce262a8fe43ea420
HL7783 <seojoon2015@daum.net>
2026-05-11 17:05:24 +0900

    asdf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, render_template, request, session, url_for
 import os
-from db import init_db, check_account, add_account
+from db import init_db, check_account, add_account, get_all_posts, create_post, \
+    get_post_by_post_id, update_post, delete_post
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -12,6 +13,108 @@ def get_index():
         return redirect(url_for('get_login'))
 
     return render_template('index.html', username=session['username'])
+
+
+
+@app.route('/posts', methods=['GET'])
+def get_posts():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+    
+    posts = get_all_posts()
+    return render_template('posts.html', posts=posts)
+
+@app.route('/posts/new', methods=['GET'])
+def get_posts_new():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+    
+    return render_template('posts_new.html')
+
+
+@app.route('/posts/new', methods=['POST'])
+def post_posts_new():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+    
+    title = request.form.get('title')
+    content = request.form.get('content')
+    author_id = session['user_id']
+    create_post(title, content, author_id)
+    return redirect(url_for('get_posts'))
+
+@app.route('/posts/<post_id>', methods=['GET'])
+def get_posts_post_id(post_id):
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+    
+    post = get_post_by_post_id(post_id)
+    if post:
+        return render_template('post.html', post=post)
+    else:
+        return redirect(url_for('get_posts'))    
+    
+@app.route('/posts/<post_id>/edit', methods=['GET'])
+def get_posts_post_id_edit(post_id):
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+    
+    post = get_post_by_post_id(post_id)
+    if not post:
+        return redirect(url_for('get_posts'))
+    
+    if post[3] != session['username']:
+        return render_template('post_edit_failure.html')
+    
+    return render_template('post_edit.html', post=post)
+    
+@app.route('/posts/<post_id>/edit', methods=['POST'])
+def post_posts_post_id_edit(post_id):
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+    
+    post = get_post_by_post_id(post_id)
+    if not post:
+        return redirect(url_for('get_posts'))
+    
+    if post[3] != session['username']:
+        return render_template('post_edit_failure.html')
+    
+    title = request.form.get('title')
+    content = request.form.get('content')
+    update_post(post_id, title, content)
+    return redirect(url_for('get_posts_post_id', post_id=post_id))
+
+@app.route('/posts/<post_id>/delete', methods=['GET'])
+def get_posts_post_id_delete(post_id):
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+
+    post = get_post_by_post_id(post_id)
+    if not post:
+        return redirect(url_for('get_posts'))
+
+    if post[3] != session['username']:
+        return render_template('post_delete_failure.html')
+
+    return render_template('post_delete.html', post=post)
+
+
+@app.route('/posts/<post_id>/delete', methods=['POST'])
+def post_posts_post_id_delete(post_id):
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+
+    post = get_post_by_post_id(post_id)
+    if not post:
+        return redirect(url_for('get_posts'))
+
+    if post[3] != session['username']:
+        return render_template('post_delete_failure.html')
+
+    delete_post(post_id)
+    return redirect(url_for('get_posts'))
+
 
 
 @app.route('/register', methods=['GET'])
@@ -50,8 +153,10 @@ def post_login():
 
     username = request.form.get('username')
     password = request.form.get('password')
-    if check_account(username, password):
-        session['username'] = username
+    user = check_account(username, password)
+    if user:
+        session['user_id'] = user[0]
+        session['username'] = user[1]
         return redirect(url_for('get_index'))
     else:
         return render_template('login_failure.html')
--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect('accounts.db')
+    conn = sqlite3.connect('webserver.db')
     cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS accounts (
@@ -10,29 +10,81 @@ def init_db():
             password    TEXT NOT NULL
         )
     ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS posts (
+            id      INTEGER PRIMARY KEY AUTOINCREMENT,
+            title   TEXT NOT NULL,
+            content TEXT NOT NULL,
+            author  TEXT NOT NULL
+        )
+    ''')
     conn.commit()
     conn.close()
 
 
 def add_account(username, password):
-    conn = sqlite3.connect('accounts.db')
+    conn = sqlite3.connect('webserver.db')
     cursor = conn.cursor()
     try:
         cursor.execute('INSERT INTO accounts (username, password) VALUES (?, ?)',
                        (username, password))
         conn.commit()
-        conn.close()
-        return True # success
+        return True 
     except sqlite3.IntegrityError:
+        return False
+    finally:
         conn.close()
-        return False # failure
 
 
 def check_account(username, password):
-    conn = sqlite3.connect('accounts.db')
+    conn = sqlite3.connect('webserver.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM accounts WHERE username = ? AND password = ?',
                    (username, password))
     user = cursor.fetchone()
     conn.close()
     return user
+
+
+def get_all_posts():
+    conn = sqlite3.connect('webserver.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM posts')
+    posts = cursor.fetchall()
+    conn.close()
+    return posts
+
+
+def create_post(title, content, author_id):
+    conn = sqlite3.connect('webserver.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO posts (title, content, author) VALUES (?, ?, ?)',
+                   (title, content, author_id))
+    conn.commit()
+    conn.close()
+
+
+def get_post_by_post_id(post_id):
+    conn = sqlite3.connect('webserver.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM posts WHERE id = ?', (post_id,))
+    post = cursor.fetchone()
+    conn.close()
+    return post
+    
+
+def update_post(post_id, new_title, new_content):
+    conn = sqlite3.connect('webserver.db')
+    cursor = conn.cursor()
+    cursor.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?',
+                   (new_title, new_content, post_id))
+    conn.commit()
+    conn.close()
+
+
+def delete_post(post_id):
+    conn = sqlite3.connect('webserver.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.commit()
+    conn.close()
