d2c6c0cfc1b39c04ce2e4fcdc2a30d279008fb03
HL7783 <seojoon2015@daum.net>
2026-05-12 14:40:39 +0900

    2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = os.urandom(32)
 @app.route('/', methods=['GET'])
 def get_index():
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
 
     return render_template('index.html', username=session['username'])
 
@@ -19,76 +19,58 @@ def get_index():
 @app.route('/posts', methods=['GET'])
 def get_posts():
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
     
     posts = get_all_posts()
     return render_template('posts.html', posts=posts)
 
-@app.route('/posts/new', methods=['GET'])
-def get_posts_new():
+@app.route('/posts/new', methods=['GET', 'POST'])
+def a_posts_new():
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
     
-    return render_template('posts_new.html')
-
-
-@app.route('/posts/new', methods=['POST'])
-def post_posts_new():
-    if 'username' not in session:
-        return redirect(url_for('get_login'))
+    if request.method =='POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        author_id = session['username']
+        create_post(title, content, author_id)
+        return redirect(url_for('get_posts'))
     
-    title = request.form.get('title')
-    content = request.form.get('content')
-    author_id = session['user_id']
-    create_post(title, content, author_id)
-    return redirect(url_for('get_posts'))
+    return render_template('post_new.html')
+
 
 @app.route('/posts/<post_id>', methods=['GET'])
 def get_posts_post_id(post_id):
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
     
     post = get_post_by_post_id(post_id)
     if post:
-        return render_template('post.html', post=post)
+        return render_template('posts.html', posts=posts)
     else:
         return redirect(url_for('get_posts'))    
     
-@app.route('/posts/<post_id>/edit', methods=['GET'])
-def get_posts_post_id_edit(post_id):
+@app.route('/posts/<post_id>/edit', methods=['GET', 'POST'])
+def edit_post(post_id):
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
     
     post = get_post_by_post_id(post_id)
-    if not post:
-        return redirect(url_for('get_posts'))
-    
-    if post[3] != session['username']:
-        return render_template('post_edit_failure.html')
-    
-    return render_template('post_edit.html', post=post)
-    
-@app.route('/posts/<post_id>/edit', methods=['POST'])
-def post_posts_post_id_edit(post_id):
-    if 'username' not in session:
-        return redirect(url_for('get_login'))
-    
-    post = get_post_by_post_id(post_id)
-    if not post:
-        return redirect(url_for('get_posts'))
-    
-    if post[3] != session['username']:
-        return render_template('post_edit_failure.html')
-    
-    title = request.form.get('title')
-    content = request.form.get('content')
-    update_post(post_id, title, content)
-    return redirect(url_for('get_posts_post_id', post_id=post_id))
+    if not post: return redirect(url_for('get_posts'))
+    if post[3] != session['username']: return render_template('post_edit_failure.html')
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        update_post(post_id, title, content)
+        return redirect(url_for('get_posts_post_id', post_id=post_id))
+
+    return render_template('post_edit.html', posts=posts)
 
 @app.route('/posts/<post_id>/delete', methods=['GET'])
 def get_posts_post_id_delete(post_id):
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
 
     post = get_post_by_post_id(post_id)
     if not post:
@@ -103,7 +85,7 @@ def get_posts_post_id_delete(post_id):
 @app.route('/posts/<post_id>/delete', methods=['POST'])
 def post_posts_post_id_delete(post_id):
     if 'username' not in session:
-        return redirect(url_for('get_login'))
+        return redirect(url_for('post_login'))
 
     post = get_post_by_post_id(post_id)
     if not post:
@@ -116,56 +98,43 @@ def post_posts_post_id_delete(post_id):
     return redirect(url_for('get_posts'))
 
 
-
-@app.route('/register', methods=['GET'])
-def get_register():
-    if 'username' in session:
-        return redirect(url_for('get_index'))
-
-    return render_template('register.html')
-
-
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST', 'GET'])
 def post_register():
     if 'username' in session:
         return redirect(url_for('get_index'))
-
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if add_account(username, password):
-        return redirect(url_for('get_login'))
-    else:
-        return render_template('register_failure.html')
-
-
-@app.route('/login', methods=['GET'])
-def get_login():
-    if 'username' in session:
-        return redirect(url_for('get_index'))
-
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if add_account(username, password):
+            return redirect(url_for('post_login'))
+        else:
+            return render_template('register_failure.html')
+    return render_template('register.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def post_login():
     if 'username' in session:
         return redirect(url_for('get_index'))
 
-    username = request.form.get('username')
-    password = request.form.get('password')
-    user = check_account(username, password)
-    if user:
-        session['user_id'] = user[0]
-        session['username'] = user[1]
-        return redirect(url_for('get_index'))
-    else:
-        return render_template('login_failure.html')
+    if request.method =='POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = check_account(username, password)
+        if user:
+            session['user_id'] = user[0]
+            session['username'] = user[1]
+            return redirect(url_for('get_index'))
+        else:
+            return render_template('login_failure.html')
+    
+    return render_template('login.html')
 
 
 @app.route('/logout', methods=['GET'])
 def get_logout():
     session.clear()
-    return redirect(url_for('get_login'))
+    return redirect(url_for('post_login'))
 
 
 if __name__ == '__main__':
